a4a06ec82c23b5dbf049201bd0d9cff84827a15d
Mahesh Mogal <mogal.mahesh33@gmail.com>
2021-08-23 20:51:57 +0530

    upload_fileobj method added to upload file data to s3

--- objects/upload_files_to_s3_bucket.py
+++ objects/upload_files_to_s3_bucket.py
@@ -57,7 +57,25 @@ def upload_file_to_s3_using_put_object():
     )  # prints s3.Object(bucket_name='binary-guy-frompython-2', key='sample_using_put_object.txt')
 
 
+def upload_file_to_s3_using_file_object():
+    """
+    Uploads to file to s3 using upload_fileobj function of s3 client object.
+    Similar function is available for s3 resource object as well.
+    In this case, instead of copying file, we open that file and copy data of that file to S3.
+    This can be useful when you want to make some validations on data before you copy that data to S3.
+    :return: None
+    """
+    s3 = boto3.client("s3")
+    bucket_name = "binary-guy-frompython-1"
+    object_name = "sample_file_object.txt"
+    file_name = f"{pathlib.Path(__file__).parent.resolve()}\\sample_file.txt"
+
+    with open(file_name, "rb") as data:
+        s3.upload_fileobj(data, bucket_name, object_name)
+
+
 if __name__ == "__main__":
     upload_file_using_client()
     upload_file_using_resource()
     upload_file_to_s3_using_put_object()
+    upload_file_to_s3_using_file_object()
